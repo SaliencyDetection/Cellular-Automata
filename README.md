6c0d667cd0b1b6420cf021748c879f3a467c901a
kunhuang <kunhuang110@126.com>
2016-05-11 19:42:07 -0700

    multilabel cellular automata

--- README.md
+++ README.md
@@ -6,16 +6,32 @@ http://cseweb.ucsd.edu/~yaq007/0873.pdf
 ### Usage
 
 #### example
-python ca_slic.py test/cat_origin.jpg test/cat_saliency.jpg test/cat_origin_after_cut.png saliencymap/refined_saliency_cat_origin.png 500 0.05 0.25 0
+
+python ca_slic.py -i source/origin_0241.jpg -sl source/saliency_label3_0241.jpg -rsl saliencymap/refined_saliency_label3_0241.png -ns 500 -fq 0.05 -bq 0.25 -fb 0.5 -bb -0.3 -d
 
 #### args
-        argv[1](str):   Image file
-        argv[2](str):   Saliency map file
-        argv[3](str):   Result image(after cut) file
-        argv[4](str):   Refined saliency map file
-        argv[5](int):   Number of segments of superpixel
-        argv[6](float): Foreground quantile
-        argv[7](float): Background quantile
-        argv[8](float): Foreground bias
-        argv[9](float): Background bias
-        argv[10](bool): Save intermediate image for debugging
+
+    usage: python ca_slic.py [-h] [-i IMAGE] [-sl SALIENCY_LIST [SALIENCY_LIST ...]]
+               [-rsl OUTPUT_SALIENCY_LIST [OUTPUT_SALIENCY_LIST ...]]
+               [-ns NUM_SEGMENTS] [-fq FG_QUANTILE] [-bq BG_QUANTILE]
+               [-fb FG_BIAS] [-bb BG_BIAS] [-d]
+
+    optional arguments:
+      -h, --help            show this help message and exit
+      -i IMAGE, --image IMAGE
+                            Image file
+      -sl SALIENCY_LIST [SALIENCY_LIST ...], --saliency_list SALIENCY_LIST [SALIENCY_LIST ...]
+                            Saliency map file list
+      -rsl OUTPUT_SALIENCY_LIST [OUTPUT_SALIENCY_LIST ...], --output_saliency_list OUTPUT_SALIENCY_LIST [OUTPUT_SALIENCY_LIST ...]
+                            Refined saliency map file (output)
+      -ns NUM_SEGMENTS, --num_segments NUM_SEGMENTS
+                            Number of segments of superpixel
+      -fq FG_QUANTILE, --fg_quantile FG_QUANTILE
+                            Foreground quantile
+      -bq BG_QUANTILE, --bg_quantile BG_QUANTILE
+                            Background quantile
+      -fb FG_BIAS, --fg_bias FG_BIAS
+                            Foreground bias
+      -bb BG_BIAS, --bg_bias BG_BIAS
+                            Background bias
+      -d, -debug            Save intermediate image for debugging
--- ca_slic.py
+++ ca_slic.py
@@ -1,7 +1,9 @@
 import os
+PI = 3.1415
 base_path = os.getcwd()+'/'
 SHOW_IMG = True
 
+import argparse
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import numpy as np
@@ -18,8 +20,6 @@ from skimage.segmentation import mark_boundaries
 from skimage.util import img_as_float
 from skimage import io
 import matplotlib.pyplot as plt
-import argparse
-
 
 def rgb2gray(rgb):
     return np.dot(rgb[...,:3], [0.299, 0.587, 0.114])
@@ -263,6 +263,89 @@ def ca(neighbors, rgbs, fg_indexs, bg_indexs, sigma_3_square=0.1, a=0.6, b=0.2, 
 
     return S
 
+def ca_multilabel(neighbors, rgbs, super_saliency_list, fg_indexs_list, bg_indexs_list, sigma_3_square=0.1, a=0.6, b=0.2, num_step=10, fg_bias=0.3, bg_bias=-0.3):
+    """
+
+    Returns:
+        super_saliency()
+    """
+    if(len(fg_indexs_list) != len(bg_indexs_list)):
+        raise Exception("The dimension of background and foreground should be the same")
+
+    n_labels = len(fg_indexs_list)
+
+    N = len(neighbors)
+
+    F = np.zeros((N, N))
+    f = lambda c1, c2: np.exp(-LA.norm(c1-c2)/(sigma_3_square))
+    func = lambda x: 1. if x > 1. else (0. if x < 0. else x)
+    scale_to_1 = lambda x: (x-np.min(x))/(np.max(x)-np.min(x)) if (np.max(x)-np.min(x))!=0 else 0
+
+    # other_indexs = [i for i in range(N) if (i not in fg_indexs and i not in bg_indexs)]
+    
+    for i in xrange(len(neighbors)):
+        for j in neighbors[i]:
+            F[j, i] = F[i, j] = f(rgbs[i,:], rgbs[j, :])
+        D = np.diag(np.asarray(np.sum(F, axis=1)).flatten())
+
+    F_star = inv(D).dot(F)
+
+    C_diag = np.asarray(np.divide(1, np.max(F, axis=1))).flatten()
+    C_max, C_min = np.max(C_diag), np.min(C_diag)
+    C_star_diag = a*(C_diag-C_min)/(C_max-C_min)+b
+    C_star = np.diag(C_star_diag)
+    
+    # S_0 = 0.5*np.ones((N, n_labels+1))
+
+    S_0 = np.asarray(super_saliency_list).transpose((1,0))
+    for j in xrange(n_labels):
+        S_0[:, j] = scale_to_1(S_0[:, j])
+    S_0 = np.append(S_0, 1-np.max(S_0, axis=1)[:, np.newaxis], axis=1)
+
+    S_new = S = S_0
+    
+    for i in xrange(num_step):
+        for j in xrange(n_labels):
+            S_new[fg_indexs_list[j], j] += fg_bias
+            # S[bg_indexs_list[j], j] += bg_bias
+
+        S_new = C_star.dot(S_new) + (np.identity(N)-C_star).dot(F_star).dot(S_new)
+
+        # TODO
+        # if len(other_indexs) > 0:
+        #     S_new[other_indexs] = scale_to_1(S_new[other_indexs])
+        # S_new = np.vectorize(func)(S_new)
+
+        S_new = scale_to_1(S_new)
+        S_new = (S_new-0.5)*math.pi
+        S_new = np.tan(S_new)
+        # avoid overflow
+
+        S_new = S_new - np.max(S_new, axis=1)[:, np.newaxis]
+        S_new = np.exp(S_new)
+        S_new = np.divide(S_new+0.0000000001, np.sum(S_new+0.0000000001, axis=1)[:, np.newaxis])
+
+        print i, "iteration"
+        print "Norm of difference:", LA.norm(S_new-S)
+        S = S_new
+
+    for i in xrange(num_step):    
+        S_new = C_star.dot(S) + (np.identity(N)-C_star).dot(F_star).dot(S)
+        S_new = scale_to_1(S_new)
+        S_new = (S_new-0.5)*math.pi
+        S_new = np.tan(S_new)
+        # avoid overflow
+
+        S_new = S_new - np.max(S_new, axis=1)[:, np.newaxis]
+        S_new = np.exp(S_new)
+        S_new = np.divide(S_new+0.0000000001, np.sum(S_new+0.0000000001, axis=1)[:, np.newaxis])
+
+        print i, "iteration"
+        print "Norm of difference:", LA.norm(S_new-S)
+        S = S_new
+
+    return S
+
 def get_saliency(labels, super_saliency):
     n_labels = len(super_saliency)
     
@@ -280,7 +363,7 @@ def cut_saliency(image, salience_indexs):
     flatten[salience_indexs, :] = 0
     return flatten.reshape(image.shape)
 
-if __name__ == '__main__':
+def main1():
     '''
     args:
         argv[1]: Image file
@@ -356,5 +439,123 @@ if __name__ == '__main__':
         plt.imshow(after_cut_img)
         plt.show()
 
+    if SAVE_INTERMEDIATE_IMG:
+        io.imsave(image_file[:-4]+'_fg_bg_image'+image_file[-4:], fg_bg_image)
+
     io.imsave(output_image_path, refined_saliency)
     io.imsave(after_cut_name, after_cut_img)
+
+def main2():
+    '''
+    Multi-label saliency refinement
+    
+    usage: python ca_slic.py [-h] [-i IMAGE] [-sl SALIENCY_LIST [SALIENCY_LIST ...]]
+               [-rsl OUTPUT_SALIENCY_LIST [OUTPUT_SALIENCY_LIST ...]]
+               [-ns NUM_SEGMENTS] [-fq FG_QUANTILE] [-bq BG_QUANTILE]
+               [-fb FG_BIAS] [-bb BG_BIAS] [-d]
+
+    optional arguments:
+      -h, --help            show this help message and exit
+      -i IMAGE, --image IMAGE
+                            Image file
+      -sl SALIENCY_LIST [SALIENCY_LIST ...], --saliency_list SALIENCY_LIST [SALIENCY_LIST ...]
+                            Saliency map file list
+      -rsl OUTPUT_SALIENCY_LIST [OUTPUT_SALIENCY_LIST ...], --output_saliency_list OUTPUT_SALIENCY_LIST [OUTPUT_SALIENCY_LIST ...]
+                            Refined saliency map file (output)
+      -ns NUM_SEGMENTS, --num_segments NUM_SEGMENTS
+                            Number of segments of superpixel
+      -fq FG_QUANTILE, --fg_quantile FG_QUANTILE
+                            Foreground quantile
+      -bq BG_QUANTILE, --bg_quantile BG_QUANTILE
+                            Background quantile
+      -fb FG_BIAS, --fg_bias FG_BIAS
+                            Foreground bias
+      -bb BG_BIAS, --bg_bias BG_BIAS
+                            Background bias
+      -d, -debug            Save intermediate image for debugging
+    '''
+    if len(sys.argv) < 3:
+        raise ValueError("Please input the image file and saliency file")
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-i', '--image', type=str, help="Image file")
+    parser.add_argument('-sl', '--saliency_list', nargs='+', type=str, help="Saliency map file list")
+    parser.add_argument('-rsl', '--output_saliency_list', nargs='+', type=str, help="Refined saliency map file (output)")
+    parser.add_argument('-ns', '--n_segments', type=int, help="Number of segments of superpixel")
+    parser.add_argument('-fq', '--fg_quantile', type=float, help="Foreground quantile")
+    parser.add_argument('-bq', '--bg_quantile', type=float, help="Background quantile")
+    parser.add_argument('-fb', '--fg_bias', type=float, help="Foreground bias")
+    parser.add_argument('-bb', '--bg_bias', type=float, help="Background bias")
+    parser.add_argument('-d', '--debug', action='store_true', help="Save intermediate image for debugging")
+
+    n_segments = parser.parse_args().n_segments
+    fg_quantile = parser.parse_args().fg_quantile
+    bg_quantile = parser.parse_args().bg_quantile
+    fg_bias = parser.parse_args().fg_bias
+    bg_bias = parser.parse_args().bg_bias
+    SAVE_INTERMEDIATE_IMG = parser.parse_args().debug
+    SHOW_IMG = False
+    
+    input_image_path = base_path+parser.parse_args().image
+    saliency_image_path_list = [base_path+saliency_image 
+                                 for saliency_image in parser.parse_args().saliency_list]
+    output_saliency_list = [base_path+output_saliency 
+                             for output_saliency in parser.parse_args().output_saliency_list]
+
+    n_labels = len(saliency_image_path_list)
+
+    image = img_as_float(io.imread(input_image_path))
+    saliency_image_list = [img_as_float(io.imread(saliency_image_path)) 
+                            for saliency_image_path in saliency_image_path_list]
+    # image = img_as_float(cv2.cvtColor(image, cv2.COLOR_BGR2LAB))
+
+    labels, neighbors, rgbs = get_superpixel(image, num_segments=n_segments)
+    super_saliency_list = [get_supersaliency(labels, saliency_image)
+                            for saliency_image in saliency_image_list]
+    saliency_list = [get_saliency(labels, super_saliency)
+                      for super_saliency in super_saliency_list]
+    
+    if SAVE_INTERMEDIATE_IMG:
+        for i in xrange(n_labels):
+            io.imsave(saliency_image_path_list[i][:-4]+'_saliency.png', saliency_list[i])
+    if SHOW_IMG:
+        for i in xrange(n_labels):
+            plt.imshow(saliency_list[i], cmap=plt.get_cmap('gray'))
+            plt.show()
+
+    # super_foreground_indexs, super_background_indexs = get_super_index(labels, foreground_indexs, background_indexs)
+
+    super_foreground_indexs_list = [get_super_foreground_indexs(super_saliency, quantile=fg_quantile)
+                                     for super_saliency in super_saliency_list]
+    super_background_indexs_list = [get_super_background_indexs(super_saliency, quantile=bg_quantile)
+                                     for super_saliency in super_saliency_list]
+    fg_bg_image_list = [get_fg_bg(labels, super_foreground_indexs_list[i], super_background_indexs_list[i])
+                         for i in xrange(len(saliency_image_path_list))]
+
+    if SAVE_INTERMEDIATE_IMG:
+        for i in xrange(n_labels):
+            io.imsave(saliency_image_path_list[i][:-4]+'_fg_bg_image.png', fg_bg_image_list[i])
+    if SHOW_IMG:
+        for i in xrange(n_labels):
+            plt.imshow(fg_bg_image_list[i], cmap=plt.get_cmap('gray'))
+            plt.show()
+
+    super_refined_saliency_array = ca_multilabel(neighbors, rgbs, super_saliency_list, super_foreground_indexs_list, super_background_indexs_list, fg_bias=fg_bias, bg_bias=bg_bias)
+
+    refined_saliency_list = []
+    for i in xrange(n_labels+1):
+        refined_saliency_list.append(get_saliency(labels, super_refined_saliency_array[:, i]))
+
+    if SHOW_IMG:
+        for i in xrange(n_labels+1):
+            plt.imshow(refined_saliency_list[i], cmap=plt.get_cmap('gray'))
+            plt.show()
+
+    for i in xrange(n_labels):
+        io.imsave(output_saliency_list[i], refined_saliency_list[i])
+
+    io.imsave(input_image_path[:-4]+'_background_saliency.png', refined_saliency_list[-1])
+
+if __name__ == '__main__':
+    main2()
